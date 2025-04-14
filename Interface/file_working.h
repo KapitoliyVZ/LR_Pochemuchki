b8b4ebd4b03c0ce5d675e5f8111580fa2821099f
KapitoliyVZ <149462322+KapitoliyVZ@users.noreply.github.com>
2025-04-15 00:58:39 +0300

    Добавил комментарии к функциям проверки открытия файла

--- Interface/file_working.h
+++ Interface/file_working.h
@@ -5,9 +5,10 @@
 #include <string>
 #include "UI_const.h"
 
-int len_txt = 4; // длина расширения .txt
+int len_txt = 4;               // длина расширения .txt
 string extension_txt = ".txt"; // расширение .txt
 
+// Проверка расширения .txt
 bool check_txt_extension(string file_name)
 {
     if (file_name.length() >= len_txt && file_name.substr(file_name.length() - len_txt) == extension_txt)
@@ -16,6 +17,7 @@ bool check_txt_extension(string file_name)
         return false; // если нет расширения
 }
 
+// Проверка, является ли файл пустым
 bool check_inputFile_is_empty(string file_name)
 {
     // true - файл пустой
@@ -23,7 +25,7 @@ bool check_inputFile_is_empty(string file_name)
 
     if (file_input.peek() == EOF) // если первый символ это конец файла
     {
-        system("cls");
+        // закрываем файл
         file_input.close();
         return true;
     }
@@ -31,45 +33,32 @@ bool check_inputFile_is_empty(string file_name)
         return false;
 }
 
-bool checkTxtFile(const std::string& filePath) {
+// Проверка, имеет ли файл расширение .txt, существует ли он, можно ли его открыть, и не пустой ли он
+bool checkTxtFile(const std::string &filePath)
+{
     // Проверка расширения .txt вручную
     std::string path = filePath;
 
-    if (!path.empty() && path.front() == '"' && path.back() == '"') {
+    if (!path.empty() && path.front() == '"' && path.back() == '"')
         path = path.substr(1, path.size() - 2); // Удаление кавычек
-    }
-
 
     if (check_txt_extension(filePath))
-    {
         return false; // Неверное расширение
-    }
 
     // Проверка, существует ли файл
     struct stat buffer;
-    if (stat(path.c_str(), &buffer) != 0) {
+    if (stat(path.c_str(), &buffer) != 0)
         return false; // Файл не существует
-    }
 
     // Открытие файла через глобальный ifstream
     file_input.open(filePath, ios_base::in);
 
-    if (!file_input.is_open()) 
-    {
+    // Проверка, удалось ли открыть файл
+    if (!file_input.is_open())
         return false; // Не удалось открыть файл
-    }
 
     if (check_inputFile_is_empty(filePath))
-    {
         return false; // файл пустой
-    }
-    
-    string text, line;
-
-    //// Чтение текста из файла
-    //while (getline(file_input, line)) {
-    //    text += line + " ";
-    //}
 
     return true; // Файл прошел все проверки
 }
