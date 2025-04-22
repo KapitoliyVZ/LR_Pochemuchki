e515951d9df0c91773f7d1fafccfd28835a3db4f
KapitoliyVZ <149462322+KapitoliyVZ@users.noreply.github.com>
2025-04-22 10:05:57 +0300

    Update Heart_of_programm.h
    unite_functions() теперь принимает еще и текст из файла в строке

--- Interface/Heart_of_program.h
+++ Interface/Heart_of_program.h
@@ -8,10 +8,11 @@ using namespace std;
 // функция-объединение для получения и передачи рифм, флагов, предложений
 void unite_functions(vector<WordData> &wordData,        // адрес вектора для хранения данных о найденных рифмах по частям речи
                      vector<vector<string>> &sentences, // адрес вектора текста с разделенными предложениями
+	                 const string str_sentences,        // строка с текста 
                      const string &word_to_compare,     // адрес слова для поиска рифм
                      bitset<8> &button_flags)           // адрес флагов для частей речи
 {
-    sentences = splitTextIntoSentences(); // разделение текста на предложения
+    sentences = splitTextIntoSentences(str_sentences); // разделение текста на предложения
 
     if (!sentences.empty())                                                  // если текст пустой, ничего не делаем
         deal_with_words(button_flags, sentences, word_to_compare, wordData); // работа с рифмами частей речи
